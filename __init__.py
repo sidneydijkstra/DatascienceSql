7568fa7a7cdc39e25379d9e35c800edc1eca0bd4
sidneydijkstra <sidney0099@hotmail.com>
2022-04-07 01:21:16 +0200

    Added folder with sql diles

--- __init__.py
+++ __init__.py
@@ -8,29 +8,29 @@ import sqlparse
 # setup sql files
 sqlRunOrder = [
     # create and fill staging database
-    #"raw_db_create.sql",
-    #"raw_db_fill.sql",
+    #"sql/raw_db_create.sql",
+    #"sql/raw_db_fill.sql",
 
     # create real database
-    "real_db_create.sql",
+    "sql/real_db_create.sql",
 
     # fill real database (here we made sure the creation is happening in the right order)
-    "real_db_create_business.sql",
-    "real_db_create_movie.sql",
-    "real_db_create_serie.sql",
-    "real_db_create_genre.sql",
-    "real_db_create_country.sql",
-    "real_db_create_biography.sql",
-    "real_db_create_movie_locations.sql",
-    "real_db_create_movie_rating.sql",
-    "real_db_create_movie_runtime.sql",
-    "real_db_create_serie_locations.sql",
-    "real_db_create_serie_rating.sql",
-    "real_db_create_serie_runtime.sql",
+    "sql/real_db_create_business.sql",
+    "sql/real_db_create_movie.sql",
+    "sql/real_db_create_serie.sql",
+    "sql/real_db_create_genre.sql",
+    "sql/real_db_create_country.sql",
+    "sql/real_db_create_biography.sql",
+    "sql/real_db_create_movie_locations.sql",
+    "sql/real_db_create_movie_rating.sql",
+    "sql/real_db_create_movie_runtime.sql",
+    "sql/real_db_create_serie_locations.sql",
+    "sql/real_db_create_serie_rating.sql",
+    "sql/real_db_create_serie_runtime.sql",
 
-    "real_db_create_actor.sql",
-    "real_db_create_actress.sql",
-    "real_db_create_writer.sql"
+    "sql/real_db_create_actor.sql",
+    "sql/real_db_create_actress.sql",
+    "sql/real_db_create_writer.sql"
 ]
 
 # load .env variables
